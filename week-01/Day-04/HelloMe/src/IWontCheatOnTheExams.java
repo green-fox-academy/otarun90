792b4fd63789952cc4fe3ddac1fbd410d345a1c7
Farkas Daniel <farkasdaniel90@gmail.com>
2020-11-20 16:46:13 +0100

    Repeating a line 100 times

---
+++ week-01/Day-04/HelloMe/src/IWontCheatOnTheExams.java
@@ -0,0 +1,12 @@
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        // Create a program that writes this line 100 times:
+        // "I won't cheat on the exam!"
+
+        int numberOfTimes = 100;
+        for (int i = 0; i <= numberOfTimes; i++) {
+            System.out.println("I won't cheat on the exam!");
+        }
+
+    }
+}
