d350d49ce6452fab009d2e179b2520d3c0ea374a
Farkas Daniel <farkasdaniel90@gmail.com>
2020-11-23 16:41:40 +0100

    Doubling function

---
+++ week-02/Day-01/src/Doubling.java
@@ -0,0 +1,17 @@
+public class Doubling {
+    public static int doublingNumber (int number) {
+        return number * 2;
+
+        // masik verzio:
+        // int result = number = *2;
+        // return result;
+        
+    }
+    public static void main(String[] args) {
+        // - Create an integer variable named `baseNum` and assign the value `123` to it
+        // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+        // - Print the result of `doubling(baseNum)`
+        int baseNum = 123;
+        System.out.println(doublingNumber(baseNum));
+    }
+}
