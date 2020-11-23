b777eafc705b3e7a8f6d43f8a2af8f6c65fda022
Farkas Daniel <farkasdaniel90@gmail.com>
2020-11-23 13:57:53 +0100

    ++ Element

---
+++ week-02/Day-01/src/incrementElement.java
@@ -0,0 +1,12 @@
+public class incrementElement {
+    public static void main(String[] args) {
+        // - Create an array variable named `numbers`
+        //   with the following content: `[1, 2, 3, 4, 5]`
+        // - Increment the third element
+        // - Print the third element
+        int[] numbers = {1, 2, 3, 4, 5};
+        numbers[2]++; // noveli az erteket 1el
+        System.out.println(numbers[2]);
+
+    }
+}
