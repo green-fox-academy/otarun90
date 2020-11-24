d1a5227fd28e22ee4ecc4894dcb20ee950d7399f
Farkas Daniel <farkasdaniel90@gmail.com>
2020-11-24 14:24:20 +0100

    Url has been fixed

---
+++ week-02/Day-02/src/UrlFixer.java
@@ -0,0 +1,19 @@
+public class UrlFixer {
+    public static void main(String[] args) {
+        String url = ("https//www.reddit.com/r/nevertellmethebots");
+        //url = url.replace ("bots", "odds");
+        //url = url.replace ("//", "://");
+        //url = url.replaceFirst("bot", "odd");
+        //url = url.replaceFirst("/", ":/");
+        String urlUpdated = "https:"+ url.substring(5,38)+"odds";
+        //System.out.println("https:"+ url.substring(5,38)+"odds");
+
+        // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+        // Also, the URL is missing a crucial component, find out what it is and insert it too!
+        // Try to solve it more than once using different String functions!
+
+        System.out.println(urlUpdated);
+
+    }
+
+}
