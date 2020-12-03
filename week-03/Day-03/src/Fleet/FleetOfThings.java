2c645dcdcdbc3fdee49df411e8fed3c1ed5e31c5
Farkas Daniel <farkasdaniel90@gmail.com>
2020-12-03 13:57:27 +0100

    Fleet of things done

---
+++ week-03/Day-03/src/Fleet/FleetOfThings.java
@@ -0,0 +1,32 @@
+package Fleet;
+
+public class FleetOfThings {
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+        // - You have the `Thing` class
+        // - You have the `Fleet` class
+        // - You have the `FleetOfThings` class with a `main` method
+        // - Download those, use those
+        // - In the main method create a fleet
+        // - Achieve this output:
+        // Create a fleet of things to have this output:
+        // 1. [ ] Get milk
+        // 2. [ ] Remove the obstacles
+        // 3. [x] Stand up
+        // 4. [x] Eat lunch
+        Thing getMilk = new Thing("Get milk");
+        Thing removeObstacles = new Thing("Remove the obstacles");
+        Thing standUp = new Thing("Stand up");
+        Thing eatLunch = new Thing("Eat lunch");
+
+        fleet.add(getMilk);
+        fleet.add(removeObstacles);
+        fleet.add(standUp);
+        fleet.add(eatLunch);
+
+        standUp.complete();
+        eatLunch.complete();
+        
+        System.out.println(fleet);
+    }
+}
