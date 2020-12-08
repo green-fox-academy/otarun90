5c0e5b98bad404505fd881dbf921411e1c155d5e
Farkas Daniel <farkasdaniel90@gmail.com>
2020-12-08 14:00:11 +0100

    Dominoes comparable added

---
+++ week-04/Day-02/src/Dominoes/Domino.java
@@ -0,0 +1,36 @@
+package Dominoes;
+import java.util.Arrays;
+
+public class Domino implements Comparable<Domino> {
+    private final int left;
+    private final int right;
+
+    public Domino(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getLeftSide() {
+        return left;
+    }
+
+    public int getRightSide() {
+        return right;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + left + ", " + right + "]";
+    }
+
+    @Override
+    public int compareTo(Domino otherDomino){
+        if (this.left < otherDomino.right){
+            return -1;
+        }else if (this.left > otherDomino.right){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
