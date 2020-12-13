cd1f91f11c271fbe285e1b84041f4e4151aac0b5
Farkas Daniel <farkasdaniel90@gmail.com>
2020-12-13 19:00:56 +0100

    Zoo still not finished

---
+++ week-04/Day-02/src/Zoo/Animals.java
@@ -0,0 +1,22 @@
+package Zoo;
+
+public abstract class Animals {
+
+    String name;
+    int age;
+
+   public void getname(){
+
+    }
+
+    public void breed(){
+
+    }
+
+
+
+    int d; //for future GLADOS this is the elapsed time in days
+
+    int elapsedTimeInDays;
+
+}
